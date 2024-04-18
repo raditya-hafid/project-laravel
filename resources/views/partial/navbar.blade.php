b1701491e4f14a76c870f8df59f4c04e41f972a7
raditya-hafid <radityahafid123@gmail.com>
2024-04-18 17:14:46 +0700

    Ganti Logo Hapus about

--- resources/views/partial/navbar.blade.php
+++ resources/views/partial/navbar.blade.php
@@ -1,19 +1,19 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light mb-4">
     <div class="container">
-      <a class="navbar-brand" href="/">InterFade</a>
+      <a class="navbar-brand" href="/blog">InterPhoto</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
-          <li class="nav-item">
+          {{-- <li class="nav-item">
             <a class="nav-link {{ ($title === "home") ? 'active' : ''  }}" href="/">Home</a>
           </li>
           <li class="nav-item">
             <a class="nav-link {{ ($title === "about") ? 'active' : ''  }}" href="/about">About</a>
-          </li>
+          </li> --}}
           <li class="nav-item">
-            <a class="nav-link {{ ($title === "blog") ? 'active' : ''  }}" href="/blog">Blog</a>
+            <a class="nav-link {{ ($title === "blog") ? 'active' : ''  }}" href="/blog">Home</a>
           </li>
           <li class="nav-item">
             <a class="nav-link {{ ($title === "categories") ? 'active' : ''  }}" href="/categories">Categories</a>
